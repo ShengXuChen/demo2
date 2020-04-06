3725a241085300ab832fcc88a032714854cb1a7f
张锦涛 <3262511980@qq.com>
2020-04-06 20:59:27 +0800

    创建了user

---
+++ src/main/java/com/User.java
@@ -0,0 +1,14 @@
+package com;
+
+public class User {
+private Integer idInteger;
+
+public Integer getIdInteger() {
+	return idInteger;
+}
+
+public void setIdInteger(Integer idInteger) {
+	this.idInteger = idInteger;
+}
+
+}
