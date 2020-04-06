839c24118a42761baec159c25635a933c4ae4321
张锦涛 <3262511980@qq.com>
2020-04-06 20:24:31 +0800

    添加nameString,和构造方法

--- src/main/java/com/Student.java
+++ src/main/java/com/Student.java
@@ -2,5 +2,19 @@ package com;
 
 public class Student {
 		private Integer id;
+		private String nameString;
+		public Integer getId() {
+			return id;
+		}
+		public void setId(Integer id) {
+			this.id = id;
+		}
+		public String getNameString() {
+			return nameString;
+		}
+		public void setNameString(String nameString) {
+			this.nameString = nameString;
+		}
+		
 		
 }
